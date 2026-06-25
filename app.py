6c2db1721699785365079a5407f61965090c998b
Bansfor <sahil@rog.lan>
2026-06-26 03:14:56 +0530

    Added home route

--- app.py
+++ app.py
@@ -6,31 +6,38 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "PDF Merger Backend is Running 🚀"
 
 @app.route("/merge", methods=["POST"])
 def merge():
 
     files = request.files.getlist("pdfs")
+
+    if len(files) == 0:
+        return "No PDF files received.", 400
+
     filename = request.form.get("filename", "merged")
 
     writer = PdfWriter()
 
     for file in files:
-
         reader = PdfReader(file)
 
         for page in reader.pages:
             writer.add_page(page)
 
-    output = filename + ".pdf"
+    output_file = f"{filename}.pdf"
 
-    with open(output, "wb") as f:
+    with open(output_file, "wb") as f:
         writer.write(f)
 
     return send_file(
-        output,
+        output_file,
         as_attachment=True,
-        download_name=output
+        download_name=output_file,
+        mimetype="application/pdf"
     )
 
 
